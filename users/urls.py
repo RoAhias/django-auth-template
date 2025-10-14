78f3e9522f58149edbbcc5387699031d2d3685f5
Rodrigo Mares <rodrigo.mares98@gmail.com>
2025-10-14 11:37:02 -0600

    feat: programar el ciclo de login y logut
    crear template home para que el usuario que ingrese tenga una pagina de inicio, los usuarios que cierren sesion o no esten logeados son redirigidos a la pagina de login automaticamente

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,18 @@
-from django.contrib.auth.views import LoginView
+# users/urls.py
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name="users/login.html"), name='login')
+    path(
+        "login/",
+        LoginView.as_view(
+            template_name="users/login.html", redirect_authenticated_user=True
+        ),
+        name="login",
+    ),
+    path(
+        "logout/",
+        LogoutView.as_view(),  # ✅ redirige automáticamente
+        name="logout"
+    ),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
 
-# Create your views here.
-def login_view(request):
-    return render(request, "users/login.html")
+# 🔐 Vista Home protegida
+@login_required
+def home_view(request):
+    """
+    Esta vista será la página principal después del login.
+    Solo usuarios autenticados pueden acceder.
+    """
+    return render(request, 'home.html')
+
+class MyLogoutView(LogoutView):
+    next_page = 'login'  # a dónde redirigir después del logout
+
+    # Permite que logout funcione con GET
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
